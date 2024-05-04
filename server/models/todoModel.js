9f1a7dc91bdf1a8a3725fd60a2defbf2be83fec0
Jaswant Yadav <85235005+jaswant04@users.noreply.github.com>
2024-05-04 12:38:59 +0530

    Update todoModel.js

--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -4,7 +4,7 @@ const todoSchema = mongoose.Schema({
     todo: {
         type: String,
         required: true,
-        maxLength: 30,
+        maxLength: 50,
         minLength: 3,
         unique: true,
     }
